2ad5847e1ff78007b5e449ac70a09c3b1e86858c
archeelux <archeelux@gmail.com>
2020-02-04 22:08:39 +0000

    added tags and datetime

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const util = require("util");
+const format = require("date-format");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
@@ -6,6 +7,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("dump", obj => {
     return util.inspect(obj);
   });
+
+  eleventyConfig.addFilter("formatDate", dte =>
+    dte ? format("dd/MM/yyyy hh:mm:ss", dte) : null
+  );
 
   return {
     dir: { input: "src", output: "dist", data: "_data" },
